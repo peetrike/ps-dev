7affab4fcec31d8f9b4f0798dfb7ec50a54b097b
Peter Wawa <meelisn@outlook.com>
2024-03-31 13:15:59 +0300

    Change report to implement computer information report
    just like HtmlWithStyle.ps1

--- DemoFiles/ch04/New-ExcelReport.ps1
+++ DemoFiles/ch04/New-ExcelReport.ps1
@@ -1,3 +1,60 @@
-﻿Get-ChildItem *.ps1 |
-    Select-Object Name, Length, LastWriteTime |
-    Export-Excel -Path files.xlsx
+﻿#Requires -Modules ImportExcel
+[CmdletBinding()]
+param (
+    $ReportFile = 'OSInfo.xlsx'
+)
+
+if (Test-Path -Path $ReportFile -PathType Leaf) {
+    Remove-Item -Path $ReportFile -Force
+}
+
+#region Collect and convert information
+$OSinfo = Get-CimInstance -ClassName Win32_OperatingSystem |
+    Select-Object 'Version', 'Caption', 'BuildNumber', 'Manufacturer'
+
+$ProcessorInfo = Get-CimInstance -ClassName Win32_Processor |
+    Select-Object 'DeviceID', 'Name', 'Caption', 'MaxClockSpeed', 'SocketDesignation', 'Manufacturer'
+
+$BiosInfo = Get-CimInstance -ClassName Win32_BIOS |
+    Select-Object 'SMBIOSBIOSVersion', 'Manufacturer', 'Name', 'SerialNumber'
+
+$DiscInfo = Get-CimInstance -ClassName Win32_LogicalDisk -Filter 'DriveType=3' |
+    Select-Object 'DeviceID', 'DriveType', 'ProviderName', 'VolumeName', 'Size', 'FreeSpace'
+
+$ServicesInfo = Get-CimInstance -ClassName Win32_Service |
+    Select-Object -Property State, Name, DisplayName, StartMode, Accept*
+#endregion
+
+$OSProps = @{
+    Path         = $ReportFile
+    AutoSize     = $true
+    WorksheetName = 'OS'
+}
+$sheetName = 'Services'
+$ServiceProps = @{
+    Path          = $ReportFile
+    AutoSize      = $true
+    AutoFilter    = $true
+    WorksheetName = $sheetName
+    TableName     = $sheetName
+    TableStyle    = 'Medium5'
+    FreezeTopRow  = $true
+}
+#region Generate report
+$OSinfo | Export-Excel @OSProps -StartColumn 1 -Title 'OS Info'
+$ProcessorInfo | Export-Excel @OSProps -StartRow 5 -Title 'Processor Info'
+$BiosInfo | Export-Excel @OSProps -StartRow 9 -Title 'BIOS Info'
+$DiscInfo | Export-Excel @OSProps -StartRow 13 -Title 'Disk Info'
+
+$document = $ServicesInfo | Export-Excel @ServiceProps -Title 'Services Info' -PassThru
+$ConditionProps = @{
+    Worksheet = $document.Workbook.Worksheets[$sheetName]
+    Range     = 'A:A'
+    RuleType  = 'Equal'
+}
+Add-ConditionalFormatting @ConditionProps -ConditionValue 'Running' -BackgroundColor LightGreen
+Add-ConditionalFormatting @ConditionProps -ConditionValue 'Stopped' -BackgroundColor Red
+Close-ExcelPackage -ExcelPackage $document -Show
+#endregion
+
+#Invoke-Item -Path $ReportFile
--- DemoFiles/ch04/New-HtmlReport.ps1
+++ DemoFiles/ch04/New-HtmlReport.ps1
@@ -1,36 +1,57 @@
 ﻿#Requires -Modules PSWriteHTML
 [CmdletBinding()]
-param (
-        [string]
-    $ProcessName = 'p*',
-        [int]
-    $WorkingSet = 200MB
-)
+param ()
 
-$Memory = 'PagedMemorySize', 'PrivateMemorySize', 'VirtualMemorySize'
-$Properties = @(
-    'Name'
-    'Id'
-    'PriorityClass'
-    'FileVersion'
-    'HandleCount'
-    'WorkingSet'
-    $Memory
-    'TotalProcessorTime'
-)
 
-$data = Get-Process $ProcessName | Select-Object -Property $Properties
+#region Collect and convert information
+$OSinfo = Get-CimInstance -ClassName Win32_OperatingSystem |
+    Select-Object 'Version', 'Caption', 'BuildNumber', 'Manufacturer'
 
-New-HTML -TitleText 'Process List' -Online -FilePath $PWD\Processes.html -ShowHTML {
-    New-HTMLPanel {
-        New-HTMLTable -DataTable $data -HideFooter -ScrollCollapse {
-            New-TableHeader -Names 'Name', 'ID' -Title 'Process Information' -Color DarkBlue -FontWeight lighter -Alignment left -BackGroundColor LightBlue
-            New-TableHeader -Names $Memory -Title 'Memory' -Color White -BackGroundColor Blue
-            New-TableHeader -Names 'Name' -BackGroundColor Red -Color WhiteSmoke
-            New-TableHeader -Names 'Id' -BackGroundColor Blue -Color White
-            New-TableHeader -Names 'PriorityClass', 'FileVersion', 'HandleCount' -BackGroundColor Gold -Color Blue
-            New-TableHeader -BackGroundColor Green -Color White -Title ([datetime]::Now)
-            New-TableCondition -Name 'WorkingSet' -ComparisonType number -Operator gt -Value $WorkingSet -Row -BackgroundColor Gray -Color White
+$ProcessorInfo = Get-CimInstance -ClassName Win32_Processor |
+    Select-Object 'DeviceID', 'Name', 'Caption', 'MaxClockSpeed', 'SocketDesignation', 'Manufacturer'
+
+$BiosInfo = Get-CimInstance -ClassName Win32_BIOS |
+    Select-Object 'SMBIOSBIOSVersion', 'Manufacturer', 'Name', 'SerialNumber'
+
+$DiscInfo = Get-CimInstance -ClassName Win32_LogicalDisk -Filter 'DriveType=3' |
+    Select-Object 'DeviceID', 'DriveType', 'ProviderName', 'VolumeName', 'Size', 'FreeSpace'
+
+$ServicesInfo = Get-CimInstance -ClassName Win32_Service |
+    Select-Object -Property State, Name, DisplayName, StartMode, Accept*
+#endregion
+
+
+#region Create HTML Report
+$ComputerName = 'Computer name: {0}' -f $env:COMPUTERNAME
+
+New-HTML -TitleText 'Computer Information Report' -Online -FilePath $PWD\OSInfo.html {
+    New-HTMLHeading -Heading h1 -HeadingText $ComputerName -Color DarkOrange
+    New-HTMLSection -Invisible {
+        New-HTMLPanel -Invisible {
+            New-HTMLSection -Invisible {
+                New-HTMLSection -HeaderText 'OS Info' {
+                    New-HTMLTable -DataTable $OSinfo -Transpose -Simplify -HideFooter -AutoSize
+                }
+                New-HTMLSection -HeaderText 'BIOS Info' {
+                    New-HTMLTable -DataTable $BiosInfo -Transpose -Simplify -HideFooter
+                }
+            }
+            New-HTMLSection -HeaderText 'Disk Info' -HeaderBackGroundColor DarkBlue {
+                New-HTMLTable -DataTable $DiscInfo -Simplify -HideFooter -AutoSize
+            }
+        }
+        New-HTMLPanel -Width '33%' {
+            New-HTMLSection -HeaderText 'Processor Info' {
+                New-HTMLTable -DataTable $ProcessorInfo -Transpose -Simplify -HideFooter
+            }
         }
     }
-}
+    New-HTMLSection -HeaderText 'Services Info' -HeaderTextSize 14 -CanCollapse {
+        New-HTMLTable -DataTable $ServicesInfo -PagingLength 25 {
+            New-HTMLTableCondition -Value 'Running' -BackgroundColor PaleGreen -ComparisonType string -Name State
+            New-HTMLTableCondition -Value 'Stopped' -BackgroundColor Salmon -ComparisonType string -Name State
+        }
+    }
+    New-HTMLText -Text ('Report Created: {0}' -f [datetime]::Now) -Color Red -FontSize 12
+} -ShowHTML
+#endregion
