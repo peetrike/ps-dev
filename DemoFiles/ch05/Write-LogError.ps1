7c31cc4f6ffad23a086e5048309f59e48805874d
Peter Wawa <meelisn@outlook.com>
2024-04-04 19:44:10 +0300

    add Write-LogError function

---
+++ DemoFiles/ch05/Write-LogError.ps1
@@ -0,0 +1,44 @@
+﻿function Write-LogError {
+    [CmdletBinding()]
+    param (
+            [Parameter(
+                Mandatory,
+                ValueFromPipeline
+            )]
+            [Management.Automation.ErrorRecord]
+        $ErrorRecord,
+        [string]
+        $LogPath = (Join-Path -Path $PWD -ChildPath 'ErrorLog.csv')
+    )
+
+    begin {
+        $PropertyList = @(
+            @{
+                Name       = 'Time'
+                Expression = { [datetime]::Now.ToString('o') }
+            }
+            @{
+                Name       = 'Message'
+                Expression = { $_.Exception.Message }
+            }
+            'FullyQualifiedErrorId'
+            @{
+                Name       = 'Command'
+                Expression = { $_.InvocationInfo.MyCommand }
+            }
+            @{
+                Name       = 'Line'
+                Expression = { $_.InvocationInfo.ScriptLineNumber }
+            }
+            'ScriptStackTrace'
+            'CategoryInfo'
+            'TargetObject'
+        )
+    }
+
+    process {
+        $ErrorRecord |
+            Select-Object -Property $PropertyList |
+            Export-Csv -Path $LogPath -NoTypeInformation -Append -Encoding utf8
+    }
+}
