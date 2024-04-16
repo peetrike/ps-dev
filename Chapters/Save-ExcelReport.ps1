aa6157bf1a4a91c6f95dc30bcdee7fbb88e86a1c
Peter Wawa <meelisn@outlook.com>
2024-04-16 09:33:43 +0300

    Add report generation scripts

---
+++ Chapters/Save-ExcelReport.ps1
@@ -0,0 +1,25 @@
+﻿#Requires -Modules ImportExcel
+
+$TabName = 'Processes'
+
+$ExportProps = @{
+    WorksheetName = $TabName
+    TableName     = $TabName
+    TableStyle    = 'Light12'
+    BoldTopRow    = $true
+    FreezeTopRow  = $true
+    AutoSize      = $true
+    ClearSheet    = $true
+}
+
+$document = Get-Process p* |
+    Select-Object -Property Name, WS, CPU, Description, StartTime |
+    Export-Excel -Path "${TabName}.xlsx" @ExportProps -PassThru
+$Sheet = $document.Workbook.Worksheets[$TabName]
+
+Set-ExcelRange -Worksheet $Sheet -Range 'B:B' -NumberFormat '#,###' -AutoFit
+Set-ExcelRange -Worksheet $Sheet -Range 'C:C' -NumberFormat '#,##0.00' -AutoFit
+Set-ExcelRange -Worksheet $Sheet -Range 'E:E' -NumberFormat 'dd MMMM HH:mm:ss' -AutoFit
+Add-ConditionalFormatting -Worksheet $Sheet -Range 'c2:c1000' -DataBarColor Blue
+
+Close-ExcelPackage -ExcelPackage $document -Show
---
+++ Chapters/Save-PdfReport.ps1
@@ -0,0 +1,12 @@
+﻿#Requires -Modules PSWritePDF
+[CmdletBinding()]
+param ()
+
+$DataTable = Get-Process p* | Select-Object -Property ProcessName, Id, StartTime
+$ReportFile = Join-Path -Path $PWD -ChildPath report.pdf
+
+New-PDF {
+    New-PDFText -Text 'PowerShell Process Report' -FontBold $true
+    New-PDFText -Text ('Created on: {0}' -f [datetime]::Now)
+    New-PDFTable -DataTable $DataTable
+} -FilePath $ReportFile -Show
