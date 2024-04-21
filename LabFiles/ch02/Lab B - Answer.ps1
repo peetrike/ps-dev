0a38f4c685d814ffa358838a8126263929e0b59e
Peter Wawa <meelisn@outlook.com>
2024-04-21 11:37:05 +0300

    Add Chapter 2 Lab B code

---
+++ LabFiles/ch02/Lab B - Answer.ps1
@@ -0,0 +1,17 @@
+﻿function Get-UserLogonInfo {
+    [CmdletBinding()]
+    param (
+            [Parameter(
+                Mandatory,
+                HelpMessage = "A filter, such as 'samAccountName -like `"admin*`"', which is used to search for users."
+            )]
+            [Alias('SearchFilter')]
+            [ValidateNotNullOrEmpty()]
+            [string]
+        $Filter,
+            [string]
+        $SearchBase
+    )
+
+    Get-ADUser @PSBoundParameters -Properties LastLogonDate, PasswordLastSet
+}
---
+++ LabFiles/ch02/Lab B - Starting Point.ps1
@@ -0,0 +1,17 @@
+﻿function Get-UserLogonInfo {
+    [CmdletBinding()]
+    param (
+            [string]
+        $Filter = "SamAccountName -eq '$env:USERNAME'",
+            [string]
+        $SearchBase
+    )
+
+    $UserParams = @{
+        Filter = $Filter
+    }
+    if ($SearchBase) {
+        $UserParams.SearchBase = $SearchBase
+    }
+    Get-ADUser @UserParams -Properties LastLogonDate, PasswordLastSet
+}
