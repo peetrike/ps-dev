95909bb57c1e773fda61e53c7a4b45af13b70893
Peter Wawa <meelisn@outlook.com>
2024-04-22 18:28:31 +0300

    Add Chapter 2 Lesson 4 demos

---
+++ DemoFiles/ch02/Lesson4/Step-02.ps1
@@ -0,0 +1,32 @@
+﻿# Add the pipeline input ByValue
+#Requires -Modules ActiveDirectory
+
+function Get-UserLogonInfo {
+    [CmdletBinding()]
+    param (
+            [Parameter(
+                Mandatory,
+                HelpMessage = "A filter, such as 'samAccountName -like `"admin*`"', which is used to search for users."
+            )]
+            [Alias('SearchFilter', 'Query')]
+            [ValidateNotNullOrEmpty()]
+            [string]
+        $Filter,
+            [Parameter(
+                ValueFromPipeline
+            )]
+            [Alias('OU')]
+            [Microsoft.ActiveDirectory.Management.ADOrganizationalUnit]
+        $OrganizationalUnit
+    )
+
+    process {
+        $UserParams = @{
+            Filter = $Filter
+        }
+        if ($OrganizationalUnit) {
+            $UserParams['SearchBase'] = $OrganizationalUnit.DistinguishedName
+        }
+        Get-ADUser @UserParams -Properties LastLogonDate, PasswordLastSet
+    }
+}
---
+++ DemoFiles/ch02/Lesson4/Step-03.ps1
@@ -0,0 +1,12 @@
+﻿# Load the function from previos step
+. .\step-02.ps1
+
+# This verifies that the original syntax works
+Get-UserLogonInfo -Filter 'samAccountName -like "admin*"'
+
+
+# Now try pipeline binding by value
+Get-ADOrganizationalUnit -Filter { Name -like 'IT' } |
+    Get-UserLogonInfo -Filter 'Name -like "a*"'
+
+Get-UserLogonInfo -Identity $env:USERNAME
