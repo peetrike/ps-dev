ca3550566de1d449695d661e921a088e909efbaa
Peter Wawa <meelisn@outlook.com>
2024-04-22 21:23:27 +0300

    Add Chapter 2 Lab C missing script

---
+++ LabFiles/ch02/Lab C - Task 1.ps1
@@ -0,0 +1,21 @@
+﻿function Get-UserLogonInfo {
+    [CmdletBinding()]
+    param (
+            [Parameter(
+                Mandatory,
+                HelpMessage = "A filter, such as 'samAccountName -like `"admin*`"', which is used to search for users."
+            )]
+            [Alias('SearchFilter', 'Query')]
+            [ValidateNotNullOrEmpty()]
+            [string]
+        $Filter,
+            [Parameter(
+                ValueFromPipeline
+            )]
+            [Alias('OU', 'SearchBase')]
+            [Microsoft.ActiveDirectory.Management.ADOrganizationalUnit]
+        $OrganizationalUnit
+    )
+
+    Get-ADUser @PSBoundParameters -Properties LastLogonDate, PasswordLastSet
+}
