4e8baf78bc950510dbdb6d8162d9f83edd48b0f7
Peter Wawa <meelisn@outlook.com>
2024-04-24 19:42:21 +0300

    Add Chapter 2 Lab F files

---
+++ LabFiles/ch02/Lab F - Answer.ps1
@@ -0,0 +1,41 @@
+﻿function Clear-TempFolder {
+    <#
+        .SYNOPSIS
+            Empties temp folders provided
+        .DESCRIPTION
+            This script empties temp folders provided
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+            [Parameter(
+                Mandatory,
+                ValueFromPipeline,
+                ValueFromPipelineByPropertyName
+            )]
+            [ValidateScript({
+                Test-Path -Path $_ -PathType Container
+            })]
+            [Alias('FullName')]
+            [string[]]
+            # Specifies path to search for temp folder
+        $Path,
+            [switch]
+            # Specifies, that temp folders from environment variables should be also processed
+        $IncludeEnvironment
+    )
+
+    begin {
+        if ($IncludeEnvironment.IsPresent) {
+            $Path += $env:TEMP, $env:TMP
+        }
+        $Path = $Path | Sort-Object -Unique
+    }
+
+    process {
+        foreach ($item in $Path) {
+            if ($PSCmdlet.ShouldProcess($item, 'Cleanup')) {
+                Get-ChildItem -Path $item | Remove-Item -Recurse -WhatIf:$false -Confirm:$false
+            }
+        }
+    }
+}
---
+++ LabFiles/ch02/Lab F - Task 1.ps1
@@ -0,0 +1,39 @@
+﻿function Clear-TempFolder {
+    <#
+        .SYNOPSIS
+            Empties temp folders provided
+        .DESCRIPTION
+            This script empties temp folders provided
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+            [Parameter(
+                Mandatory,
+                ValueFromPipeline,
+                ValueFromPipelineByPropertyName
+            )]
+            [ValidateScript({
+                Test-Path -Path $_ -PathType Container
+            })]
+            [Alias('FullName')]
+            [string[]]
+            # Specifies path to search for temp folder
+        $Path,
+            [switch]
+            # Specifies, that temp folders from environment variables should be also processed
+        $IncludeEnvironment
+    )
+
+    begin {
+        if ($IncludeEnvironment.IsPresent) {
+            $Path += $env:TEMP, $env:TMP
+        }
+        $Path = $Path | Sort-Object -Unique
+    }
+
+    process {
+        foreach ($item in $Path) {
+            Get-ChildItem -Path $item | Remove-Item -Recurse
+        }
+    }
+}
