c3b21168e63f461688c90cbfc8de8ffb357fdab6
Peter Wawa <meelisn@outlook.com>
2024-04-26 19:09:22 +0300

    Add Chapter 3 Lesson 1 demo files

---
+++ DemoFiles/ch03/Lesson1/Demo2.ps1
@@ -0,0 +1,6 @@
+﻿# Now that the module is saved in the correct location,
+# we can run the command as if it were a native shell command.
+
+Import-Module .\DemoTools.psm1
+
+Set-ComputerState -State Lock
