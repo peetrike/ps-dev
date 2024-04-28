0836dd086cc79768d074eec1eebc3274717c6439
Peter Wawa <meelisn@outlook.com>
2024-04-28 09:45:27 +0300

    Add verbose output sample

---
+++ DemoFiles/ch04/Verbose.ps1
@@ -0,0 +1,11 @@
+﻿[CmdletBinding()]
+param ()
+
+if ($PSBoundParameters.ContainsKey('Verbose')) {
+    Write-Verbose -Message 'Showing the effect of verbose output'
+    Get-CimInstance -ClassName Win32_LogicalDisk -Filter 'DriveType=3'
+}
+
+Write-Verbose -Message 'Showing the effect of suppressing verbose output'
+Get-CimInstance -ClassName Win32_LogicalDisk -Filter 'DriveType=3' -Verbose:$false
+Get-CimInstance -ClassName Win32_LogicalDisk -Filter 'DriveType=3' 4> $null
