0bf49b01c3ee486c085e35a68055befc4d9111c0
Peter Wawa <meelisn@outlook.com>
2024-04-28 09:45:50 +0300

    Add writing to event log sample

---
+++ DemoFiles/ch04/New-LogSource.ps1
@@ -0,0 +1,22 @@
+﻿#Requires -RunasAdministrator
+
+[CmdletBinding()]
+param (
+        [Parameter(Mandatory)]
+        [string]
+    $Source,
+        [ValidateScript({
+            [Diagnostics.EventLog]::Exists($_)
+        })]
+        [string]
+    $LogName = 'Application'
+)
+
+if ([Diagnostics.EventLog]::SourceExists($Source)) {
+    Write-Warning -Message ('Source already exists: {0}' -f $Source)
+} else {
+    [Diagnostics.EventLog]::CreateEventSource(
+        $myProvider,
+        $LogName
+    )
+}
