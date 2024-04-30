fd8a214d0ee962b43f76fdb6941b29f37db3f4f6
Peter Wawa <meelisn@outlook.com>
2024-04-30 18:56:50 +0300

    Rename Chapter 6 Lesson 2 demofiles folder

--- DemoFiles/ch06/Lesson2/Step-07.ps1
+++ DemoFiles/ch06/Lesson2/Step-07.ps1
@@ -1,7 +1,7 @@
-﻿# Fixed the error; put ISE in split-pane mode
-# In the ISE, set a breakpoint on line 16 and then
+﻿# Re-introducing an error and removing Write-Debug so that
+# we can look at breakpoints instead
+# In the VS Code, set a breakpoint on line 14 and then
 # press F5 to run the script
-# At the breakpoint, press F11 to continue line-by-line
 
 function Get-ServiceProcess {
     [CmdletBinding()]
@@ -13,18 +13,14 @@ function Get-ServiceProcess {
 
     $ServiceName = $ServiceName.Replace('*', '%')
     Get-CimInstance -ClassName Win32_Service -Filter "Name LIKE '$ServiceName'" -Verbose:$false | ForEach-Object {
-        if ($_.State -ne 'Running') {
-            $processName = '(Not started)'
-        } else {
-            $process = Get-Process -Id $_.ProcessId
-            $processName = $process.name
-        }
+        $process = Get-Process -Id $_.ProcessId
+
         [PSCustomObject] @{
             ComputerName = $env:COMPUTERNAME
             ServiceName  = $_.Name
-            ProcessName  = $processName
+            ProcessName  = $process.name
         }
     }
 }
 
-Get-ServiceProcess -computername localhost
+Get-ServiceProcess -ServiceName Win*
--- DemoFiles/ch06/Lesson2/Step-08.ps1
+++ DemoFiles/ch06/Lesson2/Step-08.ps1
@@ -1,7 +1,6 @@
-﻿# Re-introducing an error and removing Write-Debug so that
-# we can look at breakpoints instead
-# In the ISE, set a breakpoint on line 14 and then
+﻿# Fixed the error; in the VS Code, set a breakpoint on line 15 and then
 # press F5 to run the script
+# At the breakpoint, press F11 to continue line-by-line
 
 function Get-ServiceProcess {
     [CmdletBinding()]
@@ -13,12 +12,16 @@ function Get-ServiceProcess {
 
     $ServiceName = $ServiceName.Replace('*', '%')
     Get-CimInstance -ClassName Win32_Service -Filter "Name LIKE '$ServiceName'" -Verbose:$false | ForEach-Object {
-        $process = Get-Process -Id $_.ProcessId
-
+        if ($_.State -ne 'Running') {
+            $processName = '(Not started)'
+        } else {
+            $process = Get-Process -Id $_.ProcessId
+            $processName = $process.name
+        }
         [PSCustomObject] @{
             ComputerName = $env:COMPUTERNAME
             ServiceName  = $_.Name
-            ProcessName  = $process.name
+            ProcessName  = $processName
         }
     }
 }
