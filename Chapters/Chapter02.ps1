ddc6aaac8774df1ee11c79a273ba1f868c1660d3
Peter Wawa <meelisn@outlook.com>
2024-05-06 14:53:29 +0300

    Add Chapter 2 transcript

---
+++ Chapters/Chapter02.ps1
@@ -0,0 +1,179 @@
+﻿<#
+    .SYNOPSIS
+        Chapter 02 samples
+    .DESCRIPTION
+        This file contains sample commands from course PS-Dev for
+        Chapter 02 - Creating Functions
+    .LINK
+        https://github.com/peetrike/ps-dev
+    .LINK
+        https://diigo.com/profile/peetrike/?query=%23PS-Dev+%23M2
+#>
+
+#region Safety to prevent the entire script from being run instead of a selection
+throw "You're not supposed to run the entire script"
+#endregion
+
+#region Lesson 1 - What are functions
+
+#region What are functions?
+
+#endregion
+
+#region Understanding scripts and functions
+
+#endregion
+
+#region Using Variable Scopes
+
+#endregion
+
+#region Using Dot Sourcing
+
+#endregion
+
+#endregion
+
+
+#region Lesson 2 - Converting a Command into an Advanced Function
+
+#region Understanding Kinds of Functions
+
+#endregion
+
+#region Creating an Advanced Function
+
+#endregion
+
+#region Identifying Command Values That Can Be Parameterized
+
+#endregion
+
+#endregion
+
+
+#region Lab A - Converting a Command into an Advanced Function
+
+#endregion
+
+
+#region Lesson 3 - Defining Parameter Attributes and Input Validation
+
+#region Understanding the Parameter Attributes
+
+#endregion
+
+#region Defining a Parameter as Mandatory
+
+#endregion
+
+#region Defining a Parameter Help Message
+
+#endregion
+
+#region Defining Parameter Name Aliases
+
+#endregion
+
+#region Understanding Parameter Input Validation
+
+#endregion
+
+#endregion
+
+
+#region Lab B - Defining Parameter Attributes and Input Validation
+
+#endregion
+
+
+#region Lesson 4 - Writing Functions That Accept Pipeline Input
+
+#region Understanding Functions That Accept Pipeline Input
+
+#endregion
+
+#region Understanding Pipeline Parameter Binding
+
+#endregion
+
+#region Comparing Pipeline Execution and Parameter Execution
+
+#endregion
+
+#endregion
+
+
+#region Lab C - Writing Functions That Accept Pipeline Input
+
+#endregion
+
+
+#region Lesson 5 - Producing Complex Function Output
+
+#region Understanding the Advantages of Object Output
+
+#endregion
+
+#region Creating the Properties for a Custom Object
+
+#endregion
+
+#region Creating and Producing a Custom Object
+
+#endregion
+
+#region Adding Custom TypeName to Object
+
+#endregion
+
+#endregion
+
+
+#region Lab D - Producing Complex Function Output
+
+#endregion
+
+
+#region Lesson 6 - Documenting Functions by Using Comment-Based Help
+
+#region Understanding Comment-Based Help
+
+#endregion
+
+#region Adding Comment-Based Help to a Function
+
+#endregion
+
+#region Adding External Help to a Function
+
+#endregion
+
+#endregion
+
+
+#region Lab E - Documenting Functions by Using Comment-Based Help
+
+#endregion
+
+
+#region Lesson 7 - Supporting –WhatIf and –Confirm
+
+#region Understanding ConfirmImpact and Confirmation Parameters
+
+#endregion
+
+#region Declaring Support for –WhatIf and –Confirm
+
+#endregion
+
+#region Adding Support for –WhatIf and –Confirm
+
+#endregion
+
+#endregion
+
+
+#region Lab F - Supporting –WhatIf and –Confirm
+
+#endregion
