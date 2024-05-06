554e7f3b8ed0ccc7b1916c5d8c52a48502188e8f
Peter Wawa <meelisn@outlook.com>
2024-05-06 15:45:24 +0300

    Add Chapter 5 transcript

---
+++ Chapters/Chapter05.ps1
@@ -0,0 +1,79 @@
+﻿<#
+    .SYNOPSIS
+        Chapter 05 samples
+    .DESCRIPTION
+        This file contains sample commands from course PS-Dev for
+        Chapter 05 - Handling Errors
+    .LINK
+        https://github.com/peetrike/ps-dev
+    .LINK
+        https://diigo.com/profile/peetrike/?query=%23PS-Dev+%23M5
+#>
+
+#region Safety to prevent the entire script from being run instead of a selection
+throw "You're not supposed to run the entire script"
+#endregion
+
+
+#region Lesson 1 - Understanding Error Handling
+
+#region Comparing Different Kinds of Errors
+
+#endregion
+
+#region Understanding the Default Error Handling
+
+#endregion
+
+#region Detecting Errors
+
+#endregion
+
+#region Capturing Errors
+
+#endregion
+
+#endregion
+
+
+#region Lesson 2 - Handling Errors in a Script
+
+#region Identifying and Anticipating Operational Errors
+
+#endregion
+
+#region Adding Error Handling Code to a Script
+
+#endregion
+
+#region Logging Errors to a Text File
+
+#endregion
+
+#endregion
+
+
+#region Lesson 3 - Raising Errors in a Script
+
+#region Understanding Error stream
+
+#endregion
+
+#region Raising terminating errors
+
+#endregion
+
+#region Raising non-terminating errors
+
+#endregion
+
+#region Using Warning stream
+
+#endregion
+
+#endregion
+
+
+#region Lab - Handling Errors in a Script
+
+#endregion
