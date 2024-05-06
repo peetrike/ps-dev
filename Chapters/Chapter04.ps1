4cff1d3eaccd4dc06054df1c8fd435410fdf7225
Peter Wawa <meelisn@outlook.com>
2024-05-06 15:45:07 +0300

    Adding Chapter 4 transcript

---
+++ Chapters/Chapter04.ps1
@@ -0,0 +1,133 @@
+﻿<#
+    .SYNOPSIS
+        Chapter 04 samples
+    .DESCRIPTION
+        This file contains sample commands from course PS-Dev for
+        Chapter 04 - Writing Controller Scripts
+    .LINK
+        https://github.com/peetrike/ps-dev
+    .LINK
+        https://diigo.com/profile/peetrike/?query=%23PS-Dev+%23M4
+#>
+
+#region Safety to prevent the entire script from being run instead of a selection
+throw "You're not supposed to run the entire script"
+#endregion
+
+
+#region Lesson 1 - Understanding Controller Scripts
+
+#region Understanding Tools
+
+#endregion
+
+#region Understanding Controller Scripts
+
+#endregion
+
+#region Combining Tools and Controller Scripts
+
+#endregion
+
+#region Comparing Tools and Controller Scripts
+
+#endregion
+
+#endregion
+
+
+#region Lesson 2 - Writing Interactive Controller Scripts
+
+#region Using Write-Progress
+
+#endregion
+
+#region Using Verbose Output
+
+#endregion
+
+#region Writing to screen
+
+#endregion
+
+#region Using Read-Host
+
+#endregion
+
+#region Creating multi-choice prompt
+
+#endregion
+
+#region Using Get-Credential
+
+#endregion
+
+#region Using Out-GridView
+
+#endregion
+
+#region Using Text-based User Interface
+
+#endregion
+
+#region Using GUI elements
+
+#endregion
+
+#endregion
+
+
+#region Lab A - Writing Controller Scripts That Display a User Interface
+
+#endregion
+
+
+#region Lesson 3 - Use Logging in Controller Scripts
+
+#region Kinds of information to log
+
+#endregion
+
+#region Redirecting Message Streams
+
+#endregion
+
+#region Logging to a Text File
+
+#endregion
+
+#region Logging to Event Log
+
+#endregion
+
+#endregion
+
+
+#region Lesson 4 - Writing Controller Scripts That Perform Reporting
+
+#region Exporting data from PowerShell
+
+#endregion
+
+#region Converting command output to HTML
+
+#endregion
+
+#region Adding Basic Formatting to an HTML Page
+
+#endregion
+
+#region Converting command output to Excel
+
+#endregion
+
+#region Converting command output to PDF
+
+#endregion
+
+#endregion
+
+
+#region Lab B - Writing Controller Scripts That Produce HTML
+
+#endregion
