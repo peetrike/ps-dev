b3e952c6627db5caf5e77bb79a2a921c493b6373
Peter Wawa <meelisn@outlook.com>
2024-05-06 14:53:39 +0300

    Add Chapter 3 transcript

---
+++ Chapters/Chapter03.ps1
@@ -0,0 +1,102 @@
+﻿<#
+    .SYNOPSIS
+        Chapter 03 samples
+    .DESCRIPTION
+        This file contains sample commands from course PS-Dev for
+        Chapter 03 - Creating Modules
+    .LINK
+        https://github.com/peetrike/ps-dev
+    .LINK
+        https://diigo.com/profile/peetrike/?query=%23PS-Dev+%23M3
+#>
+
+#region Safety to prevent the entire script from being run instead of a selection
+throw "You're not supposed to run the entire script"
+#endregion
+
+
+#region Lesson 1 - Creating a Script Module
+
+#region Understanding Script Modules
+
+#endregion
+
+#region Converting a Script into a Script Module
+
+#endregion
+
+#region Testing a Script Module
+
+#endregion
+
+#endregion
+
+
+#region Lesson 2 - Creating a Module Manifest
+
+#region Understanding Module Manifest
+
+#endregion
+
+#region
+
+#endregion
+
+#region Creating Module Manifest
+
+#endregion
+
+#region Manifest fields that affect module loading
+
+#endregion
+
+#region Metadata fields in Manifest
+
+#endregion
+
+#endregion
+
+
+#region Lesson 3 - Installing Modules
+
+#region Module Autoloading
+
+#endregion
+
+#region Module folder structure
+
+#endregion
+
+#region Module search paths
+
+#endregion
+
+#region Installing Modules
+
+# https://github.com/peetrike/scripts/blob/master/src/ComputerManagement/PowerShell/Update-PowerShellGet.ps1
+
+#endregion
+
+#endregion
+
+
+#region Lesson 4 - Publishing Modules
+
+#region Module Requirements for Publishing
+
+#endregion
+
+#region Publishing Module using PowerShellGet
+
+#endregion
+
+#region Publishing Module using PSResourceGet
+
+#endregion
+
+#endregion
+
+
+#region Lab - Creating a Script Module
+
+#endregion
