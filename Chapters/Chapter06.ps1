27be3490409acfb66de1e6b216a41215ecfcbaf7
Peter Wawa <meelisn@outlook.com>
2024-05-06 16:46:20 +0300

    adding Chapter 6 transcript

---
+++ Chapters/Chapter06.ps1
@@ -0,0 +1,79 @@
+﻿<#
+    .SYNOPSIS
+        Chapter 06 samples
+    .DESCRIPTION
+        This file contains sample commands from course PS-Dev for
+        Chapter 06 - Analyzing and Debugging Scripts
+    .LINK
+        https://github.com/peetrike/ps-dev
+    .LINK
+        https://diigo.com/profile/peetrike/?query=%23PS-Dev+%23M6
+#>
+
+#region Safety to prevent the entire script from being run instead of a selection
+throw "You're not supposed to run the entire script"
+#endregion
+
+
+#region Lesson 1 - Debugging in PowerShell
+
+#region Understanding Debugging
+
+#endregion
+
+#region Displaying Debug Output
+
+#endregion
+
+#region Setting Breakpoints
+
+#endregion
+
+#region Debugging in the IDE
+
+#endregion
+
+#endregion
+
+
+#region Lesson 2 - Analyzing and Debugging an Existing Script
+
+#region Reviewing an Existing Script
+
+#endregion
+
+#region Adding Debug Code and Breakpoints to a Script
+
+#endregion
+
+#region Testing a Script and Resolving Errors
+
+#endregion
+
+#endregion
+
+
+#region Lesson 3 - Writing unit tests using Pester
+
+#region Understanding Test Driven Development
+
+#endregion
+
+#region What is Pester
+
+#endregion
+
+#region Writing tests
+
+#endregion
+
+#region Running tests
+
+#endregion
+
+#endregion
+
+
+#region Lab - Analyzing and Debugging an Existing Script
+
+#endregion
