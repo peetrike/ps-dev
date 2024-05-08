a797a9cb4593ce9b5e1d723ff9a9d4d0892246e1
Peter Wawa <meelisn@outlook.com>
2024-05-08 20:25:20 +0300

    Add chapter 5 samples

--- Chapters/Chapter05.ps1
+++ Chapters/Chapter05.ps1
@@ -23,9 +23,78 @@ throw "You're not supposed to run the entire script"
 
 #region Understanding the Default Error Handling
 
+# https://learn.microsoft.com/powershell/module/microsoft.powershell.core/about/about_preference_variables#erroractionpreference
+$ErrorActionPreference
+
+# https://github.com/peetrike/PWAddins/blob/master/src/Public/Get-EnumValue.ps1
+$ErrorActionPreference.GetType() | Get-EnumValue
+
+# https://learn.microsoft.com/powershell/module/microsoft.powershell.core/about/about_commonparameters#-erroraction
+
+#Requires -Version 7.3
+# https://learn.microsoft.com/powershell/module/microsoft.powershell.core/about/about_preference_variables?view=powershell-7.4#psnativecommanduseerroractionpreference
+$PSNativeCommandUseErrorActionPreference
+
+$Error | Get-Member
+$Error.GetType()
+Get-Member -InputObject $Error
+
+$Error.Clear()
+Get-ChildItem /doesnotexist -ErrorAction SilentlyContinue -ErrorVariable MyError
+$MyError
+
+$MyError.GetType()
+$MyError[0] -eq $Error[0]
+
 #endregion
 
 #region Detecting Errors
+
+Get-Help about_Try_Catch_Finally -ShowWindow
+
+try {
+    Get-ChildItem /doesnotexist -ErrorAction Stop
+    'Succeeded previous command'
+} catch {
+    'oops'
+}
+
+try {
+    Get-ChildItem /doesnotexist -ErrorAction Stop
+    'Succeeded previous command'
+} catch [Management.Automation.ItemNotFoundException] {
+    'a folder does not exist'
+} catch {
+    'oops'
+}
+
+try {
+    Get-ChildItem $env:windir\temp -ErrorAction Stop
+    'Succeeded previous command'
+} catch [Management.Automation.ItemNotFoundException] {
+    'a folder does not exist'
+} catch {
+    'oops'
+    $_.Exception.GetType()
+}
+
+try {
+    Get-ChildItem /doesnotexist -ErrorAction Stop
+    'Succeeded previous command'
+} catch {
+    'oops'
+} finally {
+    'always runs'
+}
+
+try {
+    Get-ChildItem . -ErrorAction Stop
+    'Succeeded previous command'
+} catch {
+    'oops'
+} finally {
+    'always runs'
+}
 
 #endregion
 
@@ -39,6 +108,42 @@ throw "You're not supposed to run the entire script"
 #region Lesson 2 - Handling Errors in a Script
 
 #region Identifying and Anticipating Operational Errors
+
+try {
+    $computers = Get-Content names.txt -ErrorAction Stop
+    foreach ($computer in $computers) {
+        try {
+            Get-CimInstance -Class Win32_BIOS -ComputerName $computer -ErrorAction Stop
+        } catch {
+            $computer | Out-File errorlog.txt -append
+        }
+    }
+} catch {
+    "Unknown error $_" | Add-Content -Path master-error-log.txt
+}
+
+function Get-BiosInfo {
+    param (
+            [Parameter(Mandatory)]
+            [ValidateScript({
+                Test-Path -Path $_ -PathType Leaf
+            }, ErrorMessage = 'File does not exist')]
+            [string]
+        $NameFile
+    )
+
+    foreach ($computer in Get-Content $NameFile) {
+        try {
+            Get-CimInstance -ClassName Win32_BIOS -ComputerName $computer
+        } catch {
+            [PSCustomObject]@{
+                ComputerName = $computer
+                Error        = $_.Exception.Message
+            } | Export-Csv errorlog.csv -Encoding utf8 -Append
+        }
+    }
+}
+Get-BiosInfo -NameFile somefile.txt
 
 #endregion
 
@@ -57,17 +162,72 @@ throw "You're not supposed to run the entire script"
 
 #region Understanding Error stream
 
+$Error[0].GetType() | Get-TypeUrl -Invoke
+$Error[0] | Get-Member
+
+    #Requires -Version 7
+Get-Error
+
 #endregion
 
 #region Raising terminating errors
+
+Get-Help about_Throw -ShowWindow
+
+function MyError {
+    [CmdletBinding()]
+    param ()
+
+    $myError = New-Object -TypeName Management.Automation.ErrorRecord -ArgumentList @(
+        [Management.Automation.RuntimeException] 'oops'
+        'CustomError'
+        [System.Management.Automation.ErrorCategory]::ObjectNotFound
+        Get-Process -Id $PID
+    )
+    $PSCmdlet.ThrowTerminatingError($myError)
+}
+try {
+    MyError
+    'this does not appear'
+} catch {
+    Get-Error
+}
 
 #endregion
 
 #region Raising non-terminating errors
 
+Get-Help Write-Error
+function MyError {
+    [CmdletBinding()]
+    param ()
+
+    $myError = New-Object -TypeName Management.Automation.ErrorRecord -ArgumentList @(
+        [Management.Automation.RuntimeException] 'oops'
+        'CustomError'
+        [System.Management.Automation.ErrorCategory]::ObjectNotFound
+        Get-Process -Id $PID
+    )
+    $PSCmdlet.WriteError($myError)
+}
+MyError; 'this still appears'
+Get-Error
+
 #endregion
 
 #region Using Warning stream
+
+function Invoke-Useful {
+    [CmdletBinding()]
+    param ()
+
+    Write-Warning -Message 'Something unusual happened'
+}
+
+Invoke-Useful -WarningVariable +warning -WarningAction SilentlyContinue
+$warning | Format-List * -Force
+$warning.InvocationInfo
+
 
 #endregion
 
