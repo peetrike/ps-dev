65749a1b1ae7cba555d80e8b1dfa0bc8c36cab79
Peter Wawa <meelisn@outlook.com>
2024-12-05 18:20:55 +0200

    break long line

--- DemoFiles/ch08/Lesson1/Demo1.ps1
+++ DemoFiles/ch08/Lesson1/Demo1.ps1
@@ -9,7 +9,8 @@ the highlighted line
 # 2. Using the System.Environment class, display the current computer name.
 [System.Environment]::MachineName
 
-# 3. Using the PowerShell $env variable, display the current computer name. This shows that .NET Framework classes often overlap shell functionality.
+# 3. Using the PowerShell $env variable, display the current computer name.
+# This shows that .NET Framework classes often overlap shell functionality.
 $env:COMPUTERNAME
 
 # 4. Display the value of the mathematical constant e.
