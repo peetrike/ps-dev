0eaf2b7aaf0a8fc884d5678aa8ea8ccb8592f98f
Peter Wawa <meelisn@outlook.com>
2024-12-05 18:06:16 +0200

    add .NET static member demo

---
+++ DemoFiles/ch08/Lesson1/Demo1.ps1
@@ -0,0 +1,16 @@
+﻿<#
+You can highlight each line and press F8 to run only
+the highlighted line
+#>
+
+# 1. Display the absolute value of -50.
+[Math]::Abs(-50)
+
+# 2. Using the System.Environment class, display the current computer name.
+[System.Environment]::MachineName
+
+# 3. Using the PowerShell $env variable, display the current computer name. This shows that .NET Framework classes often overlap shell functionality.
+$env:COMPUTERNAME
+
+# 4. Display the value of the mathematical constant e.
+[Math]::e
