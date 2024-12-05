bf9fb13d9d153735b1d8ad7aba85bc3db7ef3c8f
Peter Wawa <meelisn@outlook.com>
2024-12-05 20:32:39 +0200

    add second demo

---
+++ DemoFiles/ch08/Lesson1/Demo2.ps1
@@ -0,0 +1,28 @@
+﻿<#
+You can highlight each line and press F8 to run only
+the highlighted line
+#>
+
+# 1. Run Windows Notepad.
+notepad.exe
+
+# 2. Retrieve a reference to the instance of System.Diagnostics.Process that represents
+# the running copy of Windows Notepad.
+$note = Get-Process -Name Notepad
+
+# 3. Terminate the Windows Notepad process.
+$note.Kill()
+
+# 4. Create a new instance of the System.Data.SqlClient.SqlConnection class. Store it in $conn.
+$conn = New-Object -TypeName System.Data.SqlClient.SqlConnection
+
+# 5. Display the members of the instance.
+$conn | Get-Member
+
+# 6. Set the connection string to:
+# Server=myServerAddress;Database=myDataBase;Trusted_Connection=True;
+# Notice that there is no server at this address; you will not actually connect to a Microsoft SQL Server computer.
+$conn.ConnectionString = 'Server=myServerAddress;Database=myDataBase;Trusted_Connection=True;'
+
+# 7. Display a list of the connection’s properties and their values.
+$conn | Format-List -Property *
