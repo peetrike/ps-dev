c32dc664726e1c59cf407c3972c4e50074a54596
Peter Wawa <meelisn@outlook.com>
2024-12-16 18:59:21 +0200

    Modify demo
    - add Owning Process to netstat output
    - add types to template
    - add OwningProcess to template
    - add demo with template saved to file.

--- DemoFiles/ch04/Lesson5/Demo2/Step-02.ps1
+++ DemoFiles/ch04/Lesson5/Demo2/Step-02.ps1
@@ -8,20 +8,32 @@ $RemoteIP = (
 $null = Invoke-WebRequest -Uri ('https://{0}' -f $RemoteName)
 
 # capture netstat output
-$netstatOutput = netstat -np tcp
+$netstatOutput = netstat -nop tcp
 $netstatOutput | Select-Object -First 10
 
 $template = @'
 
 Active Connections
 
-  Proto  Local Address          Foreign Address        State
-  {Protocol*:TCP}    {LocalAddress:127.0.0.1}:{LocalPort:53119}        {RemoteAddress:201.108.52.65}:{RemotePort:443}     {State:ESTABLISHED}
-  {Protocol*:TCP}    {LocalAddress:172.16.0.40}:{LocalPort:64400}      {RemoteAddress:20.250.34.176}:{RemotePort:445}     {State:TIME_WAIT}
-  {Protocol*:TCP}    {LocalAddress:192.168.1.240}:{LocalPort:44030}    {RemoteAddress:20.250.34.176}:{RemotePort:9842}    {State:LAST_ACK}
+  Proto  Local Address          Foreign Address        State     PID
+  {Protocol*:TCP}    {[ipaddress]LocalAddress:127.0.0.1}:{[int]LocalPort:53119}        {[ipaddress]RemoteAddress:201.108.52.65}:{[int]RemotePort:443}     {State:ESTABLISHED}     {[int]OwningProcess:36804}
+  {Protocol*:TCP}    {[ipaddress]LocalAddress:172.16.0.40}:{[int]LocalPort:64400}      {[ipaddress]RemoteAddress:20.250.34.176}:{[int]RemotePort:445}     {State:TIME_WAIT}       {[int]OwningProcess:0}
+  {Protocol*:TCP}    {[ipaddress]LocalAddress:192.168.1.240}:{[int]LocalPort:44030}    {[ipaddress]RemoteAddress:20.250.34.176}:{[int]RemotePort:9842}    {State:LAST_ACK}        {[int]OwningProcess:2018}
 '@
 
 $netstatConverted = $netstatOutput | ConvertFrom-String -TemplateContent $template
 $netstatConverted | Select-Object -First 10
 
 $netstatConverted | Where-Object RemoteAddress -Like $RemoteIP
+
+# EXTRA: using saved template from file
+$templateFile = 'template.txt'
+$template | Set-Content -Path $templateFile
+$null = $netstatOutput | ConvertFrom-String -Templatefile $templateFile -UpdateTemplate
+
+Measure-Command {
+    $netstatOutput | ConvertFrom-String -Templatefile $templateFile
+}
+Measure-Command {
+    $netstatOutput | ConvertFrom-String -TemplateContent $template
+}
