ea06502ea48c39399451a9857959b823b1dce9e0
Peter Wawa <meelisn@outlook.com>
2024-12-17 20:52:57 +0200

    Add Chapter 4 file

---
+++ Chapters/Chapter04.ps1
@@ -0,0 +1,159 @@
+﻿<#
+    .SYNOPSIS
+        Chapter 04 samples
+    .DESCRIPTION
+        This file contains sample commands from course PS-Dev for
+        Chapter 04 - Extending PowerShell with .NET and REST
+    .LINK
+        https://github.com/peetrike/ps-dev
+    .LINK
+        https://diigo.com/profile/peetrike/?query=%23PS-Dev+%23M4
+#>
+
+#region Safety to prevent the entire script from being run instead of a selection
+throw "You're not supposed to run the entire script"
+#endregion
+
+
+#region Lesson 1 - Using .NET Class Library in PowerShell
+
+#region .NET classes, properties, and methods
+
+#endregion
+
+#region .NET documentation
+
+#endregion
+
+#region Using static .NET class members
+
+#endregion
+
+#region Comparing the .NET syntax to command syntax
+
+#endregion
+
+#region Instantiating classes and using instance members
+
+#endregion
+
+#region Understanding enumerations
+
+#endregion
+
+#endregion
+
+
+#region Lab A - Using .NET in PowerShell
+
+#endregion
+
+
+#region Lesson 2 - Adding custom .NET code to PowerShell scripts
+
+#region Modules with custom classes
+
+#endregion
+
+#region Loading available .NET components
+
+#endregion
+
+#region Loading .NET components from file
+
+#endregion
+
+#region Using the NuGet repositories
+
+#endregion
+
+#region Writing an inline C# code
+
+#endregion
+
+#region Using PowerShell Classes
+
+#endregion
+
+#endregion
+
+
+#region Lesson 3 - Using REST API in PowerShell
+
+#region What is REST API
+
+#endregion
+
+#region The REST API documentation
+
+#endregion
+
+#region REST API Syntax
+
+#endregion
+
+#region Invoking REST API methods by using PowerShell
+
+#endregion
+
+#endregion
+
+
+#region Lab B - Using .NET in PowerShell
+
+#endregion
+
+
+#region Lesson 4 - Reading, manipulating, and writing data in XML
+
+#region Why use XML?
+
+#endregion
+
+#region Understanding XML
+
+#endregion
+
+#region Reading XML data into memory
+
+#endregion
+
+#region Searching for XML elements
+
+#endregion
+
+#region Manipulating XML nodes
+
+#endregion
+
+#region Manipulating XML attributes
+
+#endregion
+
+#endregion
+
+
+#region Lab C - Processing XML-formatted data
+
+#endregion
+
+
+#region Lesson 5 - Reading and manipulating custom-formatted data
+
+#region Introduction to regular expressions
+
+#endregion
+
+#region Basic delimited parsing with ConvertFrom-String
+
+#endregion
+
+#region Example-driven parsing with ConvertFrom-String
+
+#endregion
+
+#region Manipulating strings with Convert-String
+
+#endregion
+
+#endregion
