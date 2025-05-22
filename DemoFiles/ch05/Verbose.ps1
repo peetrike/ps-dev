5c703b0d0749f916db6e8b0e231c5bc67e45334e
Peter Wawa <meelisn@outlook.com>
2025-05-22 21:08:40 +0300

    Add sample suppressing Verbose with splatting

--- DemoFiles/ch05/Verbose.ps1
+++ DemoFiles/ch05/Verbose.ps1
@@ -7,5 +7,14 @@ if ($PSBoundParameters.ContainsKey('Verbose')) {
 }
 
 Write-Verbose -Message 'Showing the effect of suppressing verbose output'
-Get-CimInstance -ClassName Win32_LogicalDisk -Filter 'DriveType=3' -Verbose:$false
 Get-CimInstance -ClassName Win32_LogicalDisk -Filter 'DriveType=3' 4> $null
+Get-CimInstance -ClassName Win32_LogicalDisk -Filter 'DriveType=3' -Verbose:$false
+
+$SuppressVerbose = @{ Verbose = $false }
+
+$Parameters = @{
+    ClassName = 'Win32_LogicalDisk'
+    Filter    = 'DriveType=3'
+}
+#$Parameters['NameSpace'] = 'root\cimv2'
+Get-CimInstance @Parameters @SuppressVerbose
