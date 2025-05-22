2344f1267c7157b8144c90f73379468ee2fcbcd0
Peter Wawa <meelisn@outlook.com>
2025-05-22 13:53:08 +0300

    Added Chapter 9

---
+++ Chapters/Chapter09.ps1
@@ -0,0 +1,48 @@
+﻿<#
+    .SYNOPSIS
+        Chapter 09 samples
+    .DESCRIPTION
+        This file contains sample commands from course PS-Dev for
+        Chapter 09 - Writing clean code
+    .LINK
+        https://github.com/peetrike/ps-dev
+    .LINK
+        https://diigo.com/profile/peetrike/?query=%23PS-Dev+%23M9
+#>
+
+#region Safety to prevent the entire script from being run instead of a selection
+throw "You're not supposed to run the entire script"
+#endregion
+
+#region Lesson 1 - Describing clean code
+
+#region Code Clarity
+#endregion
+
+#region Coding Conventions
+#endregion
+
+#endregion
+
+
+#region Lesson 2 - Naming suggestions
+
+#region Naming Variables
+#endregion
+
+#region Naming Functions
+#endregion
+
+#region Naming Scripts and Modules
+#endregion
+
+#endregion
+
+
+#region Lesson 3 - Code layout
+#endregion
+
+
+#region Lesson 4 - Refactoring
+
+#endregion
